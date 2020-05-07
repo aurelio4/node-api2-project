c4478e4e840f0f8b6700eee3a0fd3b401985dc52
Aurelio Arcabascio <aurelioarcabascio@gmail.com>
2020-05-07 13:20:08 -0400

    updated console log

--- src/index.js
+++ src/index.js
@@ -14,5 +14,5 @@ server.get('/', (req, res) => {
 server.use('/api/posts', postsRouter)
 
 server.listen(4000, () => {
-  console.log('server is up on port 4000')
+  console.log(`server is up on port ${port}`)
 })
